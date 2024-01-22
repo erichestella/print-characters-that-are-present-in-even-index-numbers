a6f5964c19fabc2212ba8f2be58095765ebd2b52
erichestella <erichestella05@gmail.com>
2024-01-22 20:36:30 +0800

    adding up some controls to display the even index numbers in the word

--- print word in even index numbers.py
+++ print word in even index numbers.py
@@ -6,3 +6,7 @@ word = input('Type the word you want:\n ')
 
 #adding up some display
 print('\nResult:')
+
+#it controls the word to display only that has even index numbers
+for i in range(0, len(word), 2):
+    print(word[i])
