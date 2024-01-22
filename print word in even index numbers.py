c575c69c522a8cce54975f6adbb8050d2c3a30ab
erichestella <erichestella05@gmail.com>
2024-01-22 20:33:16 +0800

    adding up some result display

--- print word in even index numbers.py
+++ print word in even index numbers.py
@@ -3,3 +3,6 @@ print('PRINTING THE WORD AT EVEN INDEX NUMBERS. \n')
 
 #adding some variable to type the word
 word = input('Type the word you want:\n ')
+
+#adding up some display
+print('\nResult:')
